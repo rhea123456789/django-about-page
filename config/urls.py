8776f4874930b0073dec2ef278017d60df4c48cd
Rhea Mae C. Aldamia <youremail@example.com>
2025-09-14 11:44:31 +0800

    Initial commit - AboutPage with Template Inheritance

---
+++ config/urls.py
@@ -0,0 +1,9 @@
+﻿from django.contrib import admin
+from django.urls import path
+from pages import views  # import your app views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),     # home page
+    path('about/', views.about, name='about'),  # about page
+]
